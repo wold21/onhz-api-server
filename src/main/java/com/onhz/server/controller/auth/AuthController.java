13ff2ee9be0c2e66845f0a214b689c20db947b7d
wold21 <wol2180@gmail.com>
2025-02-24 17:15:38 +0900

    fixed : AuthController - 의존성 주입 final 누락 수정

--- src/main/java/com/onhz/server/controller/auth/AuthController.java
+++ src/main/java/com/onhz/server/controller/auth/AuthController.java
@@ -17,8 +17,8 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class AuthController {
 
-    private UserService userService;
-    private JwtTokenService jwtTokenService;
+    private final UserService userService;
+    private final JwtTokenService jwtTokenService;
 
     @PostMapping("/signup")
     public ResponseEntity<Void> signUp(@Valid @RequestBody SignUpRequestDto signUpDto) {
