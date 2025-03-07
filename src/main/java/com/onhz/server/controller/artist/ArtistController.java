7b819fe3743dc29fcbdc73f191c09beafcd0ad71
hanseungjae <gkstmdwo100@naver.com>
2025-03-07 17:41:21 +0900

    refactor: 리뷰 관련 컨트롤러 공통 처리로 변경

--- src/main/java/com/onhz/server/controller/artist/ArtistController.java
+++ src/main/java/com/onhz/server/controller/artist/ArtistController.java
@@ -28,68 +28,6 @@ public class ArtistController {
         return ApiResponse.success(HttpStatus.OK, "success", result);
     }
 
-    @GetMapping("/{artistId}/reviews")
-    public ApiResponse<List<ArtistResponse>> getArtistReviews(
-            @PathVariable Long artistId,
-            @RequestParam(defaultValue = "0", required = false) int offset,
-            @RequestParam(defaultValue = "10", required = false) int limit,
-            @RequestParam(name = "order_by", defaultValue = "rating_count,average_rating") String orderBy) {
-        Sort sort = new Sort(orderBy);
-        List<ArtistResponse> result = null;
-        return ApiResponse.success(HttpStatus.OK, "success", result);
-    }
-
-    @GetMapping("/{artistId}/reviews/{reviewId}")
-    public ApiResponse<List<ArtistResponse>> getArtistReviewDetail(
-            @PathVariable Long artistId,
-            @PathVariable Long reviewId) {
-        List<ArtistResponse> result = null;
-        return ApiResponse.success(HttpStatus.OK, "success", result);
-    }
-
-    @PostMapping("/{artistId}/reviews")
-    public ApiResponse<List<ArtistResponse>> postArtistReview(
-            @PathVariable Long artistId,
-            @RequestParam(defaultValue = "0", required = false) int offset,
-            @RequestParam(defaultValue = "10", required = false) int limit,
-            @RequestParam(name = "order_by", defaultValue = "created_at") String orderBy) {
-        Sort sort = new Sort(orderBy);
-        List<ArtistResponse> result = null;
-        return ApiResponse.success(HttpStatus.OK, "success", result);
-    }
-
-    @PutMapping("/{artistId}/reviews/{reviewId}")
-    public ApiResponse<List<ArtistResponse>> putArtistReview(
-            @PathVariable Long artistId,
-            @PathVariable Long reviewId) {
-        List<ArtistResponse> result = null;
-        return ApiResponse.success(HttpStatus.OK, "success", result);
-    }
-
-    @DeleteMapping("/{artistId}/reviews/{reviewId}")
-    public ApiResponse<List<ArtistResponse>> deleteArtistReview(
-            @PathVariable Long artistId,
-            @PathVariable Long reviewId) {
-        List<ArtistResponse> result = null;
-        return ApiResponse.success(HttpStatus.OK, "success", result);
-    }
-
-    @PostMapping("/{artistId}/reviews/{reviewId}/like")
-    public ApiResponse<List<ArtistResponse>> postArtistReviewLike(
-            @PathVariable Long artistId,
-            @PathVariable Long reviewId) {
-        List<ArtistResponse> result = null;
-        return ApiResponse.success(HttpStatus.OK, "success", result);
-    }
-
-    @GetMapping("/{artistId}/ratings/{userId}")
-    public ApiResponse<List<ArtistResponse>> getArtistUserRatings(
-            @PathVariable Long artistId,
-            @PathVariable Long userId) {
-        List<ArtistResponse> result = null;
-        return ApiResponse.success(HttpStatus.OK, "success", result);
-    }
-
     @GetMapping("/{artistId}/tracks")
     public ApiResponse<List<ArtistResponse>> getArtistTracks(
             @PathVariable Long artistId,
--- src/main/java/com/onhz/server/controller/review/ReviewController.java
+++ src/main/java/com/onhz/server/controller/review/ReviewController.java
@@ -2,6 +2,8 @@ package com.onhz.server.controller.review;
 
 import com.onhz.server.common.enums.Review;
 import com.onhz.server.common.utils.Sort;
+import com.onhz.server.dto.request.ReviewRequest;
+import com.onhz.server.dto.response.AlbumGenreResponse;
 import com.onhz.server.dto.response.ApiResponse;
 import com.onhz.server.dto.response.ReviewResponse;
 import org.springframework.http.HttpStatus;
@@ -10,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/v1/review")
+@RequestMapping("/v1/reviews")
 public class ReviewController {
 
     @GetMapping("/")
@@ -20,6 +22,13 @@ public class ReviewController {
             @RequestParam(name = "order_by", defaultValue = "created_at") String orderBy) {
         Sort sort = new Sort(orderBy);
         List<ReviewResponse> result = null;
+        return ApiResponse.success(HttpStatus.OK, "success", result);
+    }
+
+    @GetMapping("/{reviewId}")
+    public ApiResponse<ReviewResponse> getReviewDetail(
+            @PathVariable Long reviewId) {
+        ReviewResponse result = null;
         return ApiResponse.success(HttpStatus.OK, "success", result);
     }
 
@@ -35,7 +44,7 @@ public class ReviewController {
         return ApiResponse.success(HttpStatus.OK, "success", result);
     }
 
-    @GetMapping("/{reviewType}/{entityId}/{userId}")
+    @GetMapping("/{reviewType}/{entityId}/users/{userId}")
     public ApiResponse<List<ReviewResponse>> getReviewWithUserId(
             @PathVariable Review reviewType,
             @PathVariable Long entityId,
@@ -44,4 +53,56 @@ public class ReviewController {
         return ApiResponse.success(HttpStatus.OK, "success", result);
     }
 
+    @PostMapping("/{reviewType}/{entityId}")
+    public ApiResponse createReview(
+            @PathVariable Review reviewType,
+            @PathVariable Long entityId,
+            @RequestBody ReviewRequest requestDto){
+        List<ReviewResponse> result = null;
+        return ApiResponse.success(HttpStatus.OK, "success", result);
+    }
+
+    @PutMapping("/{reviewType}/{entityId}/{reviewId}")
+    public ApiResponse<Void> putReview(
+            @PathVariable Review reviewType,
+            @PathVariable Long entityId,
+            @PathVariable Long reviewId,
+            @RequestBody ReviewRequest requestDto) {
+        return ApiResponse.success(HttpStatus.OK, "success", null);
+    }
+
+    @DeleteMapping("/{reviewType}/{entityId}/{reviewId}")
+    public ApiResponse<Void> deleteReview(
+            @PathVariable Review reviewType,
+            @PathVariable Long entityId,
+            @PathVariable Long reviewId) {
+        return ApiResponse.success(HttpStatus.OK, "success", null);
+    }
+
+    @PostMapping("/{reviewType}/{entityId}/{reviewId}/like")
+    public ApiResponse<Void> postReviewLike(
+            @PathVariable Review reviewType,
+            @PathVariable Long entityId,
+            @PathVariable Long reviewId) {
+        return ApiResponse.success(HttpStatus.OK, "success", null);
+    }
+
+    @GetMapping("/{reviewType}/{entityId}/{reviewId}/ratings/{userId}")
+    public ApiResponse<Integer> getUserRatings(
+            @PathVariable Review reviewType,
+            @PathVariable Long entityId,
+            @PathVariable Long reviewId,
+            @PathVariable Long userId) {
+        Integer rating = null;
+        return ApiResponse.success(HttpStatus.OK, "success", rating);
+    }
+
+
+    @GetMapping("/{reviewType}/{entityId}/summary")
+    public ApiResponse<List<AlbumGenreResponse>> getReviewsSummary(
+            @PathVariable Review reviewType,
+            @PathVariable Long entityId) {
+        List<AlbumGenreResponse> result = null;
+        return ApiResponse.success(HttpStatus.OK, "success", result);
+    }
 }
