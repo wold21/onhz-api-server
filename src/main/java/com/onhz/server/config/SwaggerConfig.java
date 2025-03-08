16ad78c959f4fb6392f9e24dd5db50f1e239d2e2
hanseungjae <gkstmdwo100@naver.com>
2025-03-08 09:48:09 +0900

    feat: SwaggerConfig - Authorization 추가

--- src/main/java/com/onhz/server/config/SwaggerConfig.java
+++ src/main/java/com/onhz/server/config/SwaggerConfig.java
@@ -3,6 +3,8 @@ package com.onhz.server.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,13 +13,21 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
-                .components(new Components())
-                .info(apiInfo());
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                                .type(SecurityScheme.Type.APIKEY)
+                                .in(SecurityScheme.In.HEADER)
+                                .name("Authorization")
+                                .scheme("bearer")
+                                .bearerFormat("JWT"))
+                )
+                .info(apiInfo())
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
     }
 
     private Info apiInfo() {
         return new Info()
-                .title("Spring Boot REST API Specifications")
+                .title("oz-hz REST API")
                 .description("Specification")
                 .version("1.0.0");
     }
