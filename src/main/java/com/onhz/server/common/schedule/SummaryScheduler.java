632359cb66c070734d14bcd82e2af8c05fe15969
wold21 <wol2180@gmail.com>
2025-03-10 16:01:11 +0900

    etc : 스케쥴 주석 누락

--- src/main/java/com/onhz/server/common/schedule/SummaryScheduler.java
+++ src/main/java/com/onhz/server/common/schedule/SummaryScheduler.java
@@ -9,27 +9,27 @@ import org.springframework.stereotype.Component;
 //@RequiredArgsConstructor
 public class SummaryScheduler {
 
-    @Scheduled(cron = "0 0 0/4 * * ?")
-    public void updateUserSummary() {
-        log.info("updateUserSummary start");
-        log.info("updateUserSummary end");
-    }
-
-    @Scheduled(cron = "0 0 0/4 * * ?")
-    public void updateArtistSummary() {
-        log.info("updateArtistSummary start");
-        log.info("updateArtistSummary end");
-    }
-
-    @Scheduled(cron = "0 0 0/4 * * ?")
-    public void updateAlbumSummary() {
-        log.info("updateAlbumSummary start");
-        log.info("updateAlbumSummary end");
-    }
-
-    @Scheduled(cron = "0 0 0/4 * * ?")
-    public void updateTrackSummary() {
-        log.info("updateTrackSummary start");
-        log.info("updateTrackSummary end");
-    }
+//    @Scheduled(cron = "0 0 0/4 * * ?")
+//    public void updateUserSummary() {
+//        log.info("updateUserSummary start");
+//        log.info("updateUserSummary end");
+//    }
+//
+//    @Scheduled(cron = "0 0 0/4 * * ?")
+//    public void updateArtistSummary() {
+//        log.info("updateArtistSummary start");
+//        log.info("updateArtistSummary end");
+//    }
+//
+//    @Scheduled(cron = "0 0 0/4 * * ?")
+//    public void updateAlbumSummary() {
+//        log.info("updateAlbumSummary start");
+//        log.info("updateAlbumSummary end");
+//    }
+//
+//    @Scheduled(cron = "0 0 0/4 * * ?")
+//    public void updateTrackSummary() {
+//        log.info("updateTrackSummary start");
+//        log.info("updateTrackSummary end");
+//    }
 }
---
+++ src/test/java/com/onhz/server/service/album/AlbumServiceTest.java
@@ -0,0 +1,64 @@
+package com.onhz.server.service.album;
+
+import com.onhz.server.dto.response.AlbumGenreResponse;
+import com.onhz.server.dto.response.GenreResponse;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@SpringBootTest
+@ActiveProfiles("dev")
+@Transactional(readOnly = true)
+public class AlbumServiceTest {
+    @Autowired
+    private AlbumService albumService;
+
+    void resultToString(AlbumGenreResponse album){
+        System.out.println("- 앨범 정보\t(albumId / title / releaseDate / createdAt / coverPath)");
+        System.out.println("\t\t\t" + String.format("%d / %s / %s / %s / %s",
+                album.getId(),
+                album.getTitle(),
+                album.getReleaseDate(),
+                album.getCreatedAt(),
+                album.getCoverPath()));
+
+        System.out.println("- 장르 정보\t(genreId / code / name");
+        for(GenreResponse genre : album.getGenres()){
+            System.out.println("\t\t\t" + String.format("%d / %s / %s",
+                    genre.getId(),
+                    genre.getCode(),
+                    genre.getName()));
+        }
+        System.out.println("\n");
+    }
+
+    
+    @Test
+    @DisplayName("앨범조회")
+    void getAlbums(){
+        //given
+        int offset = 0;
+        int limit = 5;
+        String orderBy = "created_at";
+
+        List<AlbumGenreResponse> result =  albumService.getAlbums(offset, limit, orderBy);
+        assert(!result.isEmpty());
+        assert(result.size() == limit);
+
+        for(AlbumGenreResponse album : result){
+            resultToString(album);
+        }
+
+    }
+
+    @Test
+    @DisplayName("인기 앨범 조회")
+    void getAlbumWithRating(){
+        List<AlbumGenreResponse> result = albumService.getAlbums(0, 5, "rating_count,average_rating");
+    }
+}
