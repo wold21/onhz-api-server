e40f8d3dd82218a9af422a940b98bec8e2e48825
wold21 <wol2180@gmail.com>
2025-03-14 14:23:37 +0900

    fix : base api명 누락 수정

--- src/main/java/com/onhz/server/controller/common/CodeController.java
+++ src/main/java/com/onhz/server/controller/common/CodeController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/v1/code")
+@RequestMapping("/api/v1/code")
 @RequiredArgsConstructor
 public class CodeController {
     private final CodeService codeService;
