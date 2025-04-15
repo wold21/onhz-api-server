89ac2a9b028393d56931cfc35f15ebcd3142d4be
wold21 <wol2180@gmail.com>
2025-04-15 13:41:22 +0900

    update : 메인 페이지 장르 조회 시 인기 앨범 없는 장르가 조회됨

--- src/main/java/com/onhz/server/repository/dsl/GenreFeatureDSLRepositoryImpl.java
+++ src/main/java/com/onhz/server/repository/dsl/GenreFeatureDSLRepositoryImpl.java
@@ -6,10 +6,9 @@ import com.onhz.server.entity.QGenreFeaturedEntity;
 import com.onhz.server.entity.album.QAlbumEntity;
 import com.onhz.server.entity.album.QAlbumGenreEntity;
 import com.onhz.server.entity.album.QAlbumRatingSummaryEntity;
-import com.querydsl.core.Tuple;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.JPAExpressions;
-import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -37,10 +36,45 @@ public class GenreFeatureDSLRepositoryImpl  implements GenreFeatureDSLRepository
                         .innerJoin(qGenreEntity).on(qAlbumGenreEntity.genre.id.eq(qGenreEntity.id))
                         .where(
                                 qAlbumRatingSummaryEntity.averageRating.isNotNull(),
-                                qGenreEntity.code.like(qGenreFeaturedEntity.code.concat("%"))
+                                genreMatchingCondition(qGenreEntity, qGenreFeaturedEntity)
                         )
                         .exists())
                 .orderBy(Expressions.numberTemplate(Double.class, "function('random')").asc())
                 .fetch();
+    }
+
+    private BooleanExpression genreMatchingCondition(QGenreEntity genre, QGenreFeaturedEntity featuredGenre) {
+        return featuredGenre.code.lower().eq("pop")
+                .and(
+                        genre.code.lower().eq("pop")
+                                .or(genre.code.lower().like("% pop"))
+                                .or(genre.code.lower().like("%-pop"))
+                                .or(genre.code.lower().eq("j-pop"))
+                                .and(genre.code.lower().notLike("%k-pop%"))
+                )
+                .or(
+                        featuredGenre.code.lower().in("hip hop", "hiphop")
+                                .and(
+                                        genre.code.lower().eq("hip hop")
+                                                .or(genre.code.lower().eq("hiphop"))
+                                                .or(genre.code.lower().like("% hip hop"))
+                                                .or(genre.code.lower().like("% hiphop"))
+                                )
+                )
+                .or(
+                        featuredGenre.code.lower().in("kpop", "k-pop")
+                                .and(
+                                        genre.code.lower().eq("kpop")
+                                                .or(genre.code.lower().eq("k-pop"))
+                                )
+                )
+                .or(
+                        featuredGenre.code.lower().notIn("pop", "hip hop", "hiphop", "kpop", "k-pop")
+                                .and(
+                                        genre.code.lower().eq(featuredGenre.code.lower())
+                                                .or(genre.code.lower().like("% " + featuredGenre.code.lower().toString()))
+                                                .or(genre.code.lower().like("%-" + featuredGenre.code.lower().toString()))
+                                )
+                );
     }
 }
