83054324861867044bafe8d9660075d2ee0cd1c2
wold21 <wol2180@gmail.com>
2025-04-27 14:48:46 +0900

    fix : 문자열 null 처리

--- src/main/java/com/onhz/server/config/WebConfig.java
+++ src/main/java/com/onhz/server/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.onhz.server.config;
 import com.onhz.server.interceptor.SessionUpdateInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -21,5 +22,27 @@ public class WebConfig implements WebMvcConfigurer{
     @Override
     public void addFormatters(FormatterRegistry registry) {
         registry.addConverter(new ReviewEnumConverter());
+        registry.addConverter(new StringToLongConverter());
+        registry.addConverter(new StringToIntegerConverter());
+    }
+
+    public static class StringToLongConverter implements Converter<String, Long> {
+        @Override
+        public Long convert(String source) {
+            if (source == null || source.isEmpty() || "null".equals(source)) {
+                return null;
+            }
+            return Long.valueOf(source);
+        }
+    }
+
+    public static class StringToIntegerConverter implements Converter<String, Integer> {
+        @Override
+        public Integer convert(String source) {
+            if (source == null || source.isEmpty() || "null".equals(source)) {
+                return null;
+            }
+            return Integer.valueOf(source);
+        }
     }
 }
