5f284f75176060e6bf182de9479dc7eed3bf4f8f
wold21 <wol2180@gmail.com>
2025-04-28 20:24:53 +0900

    update : 소셜 로그인 실패 시 리다이렉트 처리

--- src/main/java/com/onhz/server/security/oauth/OAuth2AuthenticationFailureHandler.java
+++ src/main/java/com/onhz/server/security/oauth/OAuth2AuthenticationFailureHandler.java
@@ -36,23 +36,22 @@ public class OAuth2AuthenticationFailureHandler implements AuthenticationFailure
                 errorMessage,
                 HttpStatus.UNAUTHORIZED
         );
-        String errorJson = objectMapper.writeValueAsString(errorResponse);
 
+        String redirectUrl = "/oauth/error?message=" + errorMessage + "&status="+ HttpStatus.UNAUTHORIZED.value();
+
+        response.setContentType("text/html;charset=UTF-8");
         String html = String.format("""
-        <html>
-        <body>
-            <script>
-                window.opener.postMessage({
-                    type: 'oauth2Error',
-                    error: %s
-                }, '*');
-                window.close();
-            </script>
-        </body>
-        </html>
-        """,
-                errorJson
-        );
+            <html>
+            <body>
+                <script>
+                    // 부모 창에서 에러 페이지로 리다이렉트
+                    window.opener.location.href = '%s';
+                    // 팝업 창 닫기
+                    window.close();
+                </script>
+            </body>
+            </html>
+            """, redirectUrl);
 
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
         response.setCharacterEncoding("UTF-8");
